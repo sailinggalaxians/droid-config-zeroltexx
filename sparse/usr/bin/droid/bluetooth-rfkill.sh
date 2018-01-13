6f95119d67ae5a8bd14d654b06838b28fde72137
Kristoffer Grundström <35384754+sailinggalaxians@users.noreply.github.com>
2018-01-13 01:48:35 +0100

    Update bluetooth-rfkill.sh

--- sparse/usr/bin/droid/bluetooth-rfkill.sh
+++ sparse/usr/bin/droid/bluetooth-rfkill.sh
@@ -4,6 +4,6 @@ if [[ ${RFKILL_STATE} == 0 || ${RFKILL_STATE} == 2 ]];then
     killall brcm_patchram_plus
 else
     if [[ $(`ps aux | grep [b]rcm`|wc -l) == 0 ]];then
-        /usr/sbin/brcm_patchram_plus --patchram /vendor/firmware/bcm4335.hcd --enable_hci --bd_addr $(/bin/cat /efs/bluetooth/bt_addr) --no2bytes --tosleep 6000 /dev/ttyHS0
+        /usr/sbin/brcm_patchram_plus --patchram /vendor/firmware/bcm4773.hcd --enable_hci --bd_addr $(/bin/cat /efs/bluetooth/bt_addr) --no2bytes --tosleep 6000 /dev/ttyHS0
     fi
 fi
